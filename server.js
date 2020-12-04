4e1a5bd036f79de42ccb7bf4ab0eb2f696ef1638
rushiwadkar007 <rwadkar920@gmail.com>
2020-12-04 18:57:48 +0530

    added cart page, auth folder

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express'); // import express module.
 
-const app = express(); //to create express server object
+const app = express(); //to create express server object.
 
 const ejs=require('ejs'); //to configure template engines.
 
@@ -10,14 +10,9 @@ const path= require('path')  // to configure paths
 
 const PORT = process.env.PORT || 3000
 
-
-
 //home page
 app.use(express.static('public'));
 
-app.get('/',(req, res)=>{
-    res.render('home');
-});
 
 
 //set template engine
@@ -35,6 +30,22 @@ else{
     PORT=3000
 }
 */
+
+app.get('/',(req, res)=>{
+    res.render('home.ejs');
+});
+
+app.get('/cart',(req, res)=>{
+    res.render('customers/cart');
+});
+
+app.get('/login', (req,res)=>{
+    res.render('auth/login');
+});
+
+app.get('/register', (req, res)=>{
+    res.render('auth/register');
+})
 
 app.listen(3000, ()=>{
     console.log(`Listening on port ${PORT}`);
